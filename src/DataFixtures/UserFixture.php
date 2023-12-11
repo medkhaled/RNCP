fda17fd6ae09b60b822b1e16c25c9952336c9e75
Mohamed Khaled <medkhaledgi@gmail.com>
2023-12-11 18:10:12 +0100

    User Entity + registration

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker;
+class UserFixture extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        
+    ){}
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail('medkhaledgi@gmail.com');
+        $admin->setLastname('Khaled');
+        $admin->setFirstname('Mohamed');
+        $admin->setAddress('12 rue du port');
+        $admin->setZipcode('75011');
+        $admin->setCity('Paris');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'admin')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+        $faker = Faker\Factory::create('fr_FR');
+        for($emp = 1; $emp <= 5; $emp++){
+            $employee = new User();
+            $employee->setEmail($faker->email);
+            $employee->setLastname($faker->lastName);
+            $employee->setFirstname($faker->firstName);
+            $employee->setAddress($faker->streetAddress);
+            $employee->setZipcode(str_replace(' ', '', $faker->postcode));
+            $employee->setCity($faker->city);
+            $employee->setPassword(
+                $this->passwordEncoder->hashPassword($employee, 'secret')
+            );
+            $employee->setRoles(['ROLE_EMPLOYEE']);
+            $manager->persist($employee);
+        }
+
+        for($usr = 1; $usr <= 50; $usr++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setAddress($faker->streetAddress);
+            $user->setZipcode(str_replace(' ', '', $faker->postcode));
+            $user->setCity($faker->city);
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($user, 'user')
+            );
+            $manager->persist($user);
+        }
+       
+       
+        $manager->flush();
+    }
+}
